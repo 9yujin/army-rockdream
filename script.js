677c0ca981c71c7ecf45466a17143ca1f4fb72dd
9yujin <david0218@naver.com>
2021-08-31 01:05:34 +0900

    210831

--- script.js
+++ script.js
@@ -21,20 +21,39 @@ function searchDB(str) {
 
 function searchButton(isitsearch) {
 
-    if (isitsearch == true) {
+    if (isitsearch == true) {   //찾아보기
         var str = documnet.getElementById("SearchInput").value;
         var arr = searchDB(str);
         var parent = document.getElementById("SearchResult");
         var pl = document.getElementById("PlayList");
 
         while(SearchResult.firstChild){
-            SearchResult.removeChild(SearchResult.lastChld);
+            SearchResult.removeChild(SearchResult.lastChild);
         }
     }
-    else {
+    else {                      //재생목록
         var arr = [];
         var target = document.getElementById("savedpl");
         var selectedpl = savedpl.options[target.selectedIndex].text;
         var pl = document.getElementById("PlayList");
+
+        for (let a=0; a<plarr.length; a++) {
+            var x = plarr[a][plarr[a].length-1];
+            if (x === selectedpl) {
+                for (let b=0; b<plarr[a].length-1; b++) {
+                    var str = plarr[a][b];
+                    for (var c = 0; c<db.length; c++) {
+                        if (db[c][0] === str) {
+                            arr.push(c);
+                        }
+                    }
+                }
+            }
+        }
+        while (PlayList.firstChild) {
+            PlayList.removeChild(PlayList.lastChild);
+        }
     }
+
+
 }
